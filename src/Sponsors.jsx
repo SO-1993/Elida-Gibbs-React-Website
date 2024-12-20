79e202f2c962cb8d2d84c2e950aeae18ab151c9f
Scott <scottobrien93@outlook.com>
2024-12-20 11:42:42 +0000

    Added Brudenell image to Sponsors page

--- src/Sponsors.jsx
+++ src/Sponsors.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Sponsors() {
-  return <p>This is the sponsors page.</p>;
+  return (
+    <>
+      <p>We are proud to be sponsored by Brudenell Social Club.</p>
+      <img
+        src="utils/images/brudenell_social_club_logo.jpg"
+        alt="Brudenell Social Club logo"
+      />
+    </>
+  );
 }
 
 export default Sponsors;
