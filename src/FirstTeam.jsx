0861ef823dff1055fb21ee05f09313eedc1684d0
Scott <scottobrien93@outlook.com>
2025-01-02 12:11:41 +0000

    implemented and styled defender, midfielder, attacker avatars

--- src/FirstTeam.jsx
+++ src/FirstTeam.jsx
@@ -32,12 +32,204 @@ function FirstTeam() {
       </div>
       <div className="defenders">
         <h2>Defenders</h2>
+        <div className="defender-list">
+          <div className="defender">
+            <p>Tom Hammond</p>
+            <img
+              src="utils/images/defender-placeholder.jpg"
+              alt="defender-placeholder-image"
+              className="defender-avatar"
+            />
+          </div>
+          <div className="defender">
+            <p>Joe Butler</p>
+            <img
+              src="utils/images/defender-placeholder.jpg"
+              alt="defender-placeholder-image"
+              className="defender-avatar"
+            />
+          </div>
+          <div className="defender">
+            <p>Scott O'Brien</p>
+            <img
+              src="utils/images/defender-placeholder.jpg"
+              alt="defender-placeholder-image"
+              className="defender-avatar"
+            />
+          </div>
+          <div className="defender">
+            <p>Dan Schackleton</p>
+            <img
+              src="utils/images/defender-placeholder.jpg"
+              alt="defender-placeholder-image"
+              className="defender-avatar"
+            />
+          </div>
+          <div className="defender">
+            <p>Joshua Barnett</p>
+            <img
+              src="utils/images/defender-placeholder.jpg"
+              alt="defender-placeholder-image"
+              className="defender-avatar"
+            />
+          </div>
+          <div className="defender">
+            <p>Cameron Robinson</p>
+            <img
+              src="utils/images/defender-placeholder.jpg"
+              alt="defender-placeholder-image"
+              className="defender-avatar"
+            />
+          </div>
+          <div className="defender">
+            <p>Jamie Fozard</p>
+            <img
+              src="utils/images/defender-placeholder.jpg"
+              alt="defender-placeholder-image"
+              className="defender-avatar"
+            />
+          </div>
+          <div className="defender">
+            <p>Tim Malkin</p>
+            <img
+              src="utils/images/defender-placeholder.jpg"
+              alt="defender-placeholder-image"
+              className="defender-avatar"
+            />
+          </div>
+        </div>
       </div>
+
       <div className="midfielders">
         <h2>Midfielders</h2>
+        <div className="midfielder-list">
+          <div className="midfielder">
+            <p>Danny Kreft</p>
+            <img
+              src="utils/images/midfielder-placeholder.jpg"
+              alt="midfielder-placeholder-image"
+              className="midfielder-avatar"
+            />
+          </div>
+          <div className="midfielder">
+            <p>Dan Edmonds</p>
+            <img
+              src="utils/images/midfielder-placeholder.jpg"
+              alt="midfielder-placeholder-image"
+              className="midfielder-avatar"
+            />
+          </div>
+          <div className="midfielder">
+            <p>Dean Worrall (C)</p>
+            <img
+              src="utils/images/midfielder-placeholder.jpg"
+              alt="midfielder-placeholder-image"
+              className="midfielder-avatar"
+            />
+          </div>
+          <div className="midfielder">
+            <p>Adam Franklin</p>
+            <img
+              src="utils/images/midfielder-placeholder.jpg"
+              alt="midfielder-placeholder-image"
+              className="midfielder-avatar"
+            />
+          </div>
+          <div className="midfielder">
+            <p>Jacob Darkes</p>
+            <img
+              src="utils/images/midfielder-placeholder.jpg"
+              alt="midfielder-placeholder-image"
+              className="midfielder-avatar"
+            />
+          </div>
+          <div className="midfielder">
+            <p>Arjun Varma</p>
+            <img
+              src="utils/images/midfielder-placeholder.jpg"
+              alt="midfielder-placeholder-image"
+              className="midfielder-avatar"
+            />
+          </div>
+          <div className="midfielder">
+            <p>Aran Harris</p>
+            <img
+              src="utils/images/midfielder-placeholder.jpg"
+              alt="midfielder-placeholder-image"
+              className="midfielder-avatar"
+            />
+          </div>
+          <div className="midfielder">
+            <p>Jai Narayan</p>
+            <img
+              src="utils/images/midfielder-placeholder.jpg"
+              alt="midfielder-placeholder-image"
+              className="midfielder-avatar"
+            />
+          </div>
+          <div className="midfielder">
+            <p>Cameron Liddell</p>
+            <img
+              src="utils/images/midfielder-placeholder.jpg"
+              alt="midfielder-placeholder-image"
+              className="midfielder-avatar"
+            />
+          </div>
+          <div className="midfielder">
+            <p>Harry Whittaker</p>
+            <img
+              src="utils/images/midfielder-placeholder.jpg"
+              alt="midfielder-placeholder-image"
+              className="midfielder-avatar"
+            />
+          </div>
+        </div>
       </div>
+
       <div className="attackers">
         <h2>Attackers</h2>
+        <div className="attacker-list">
+          <div className="attacker">
+            <p>Oli Baron</p>
+            <img
+              src="utils/images/attacker-placeholder.jpg"
+              alt="attacker-placeholder-image"
+              className="attacker-avatar"
+            />
+          </div>
+          <div className="attacker">
+            <p>Jordan Moore</p>
+            <img
+              src="utils/images/attacker-placeholder.jpg"
+              alt="attacker-placeholder-image"
+              className="attacker-avatar"
+            />
+          </div>
+          <div className="attacker">
+            <p>Jordan Mercer</p>
+            <img
+              src="utils/images/attacker-placeholder.jpg"
+              alt="attacker-placeholder-image"
+              className="attacker-avatar"
+            />
+          </div>
+          <div className="attacker">
+            <p>Andy Pye</p>
+            <img
+              src="utils/images/attacker-placeholder.jpg"
+              alt="attacker-placeholder-image"
+              className="attacker-avatar"
+            />
+          </div>
+          <div className="attacker">
+            <p>Josh Robinson</p>
+            <img
+              src="utils/images/attacker-placeholder.jpg"
+              alt="attacker-placeholder-image"
+              className="attacker-avatar"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
