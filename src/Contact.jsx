a7fdf3eff78d8f6b665997bf329d8e574da5f76d
Scott <scottobrien93@outlook.com>
2025-01-04 12:00:43 +0000

    added info to Contact.jsx page

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 function Contact() {
-  return <p>This is the join page.</p>;
+  return (
+    <>
+      <h1>Contact Elida Gibbs AFC</h1>
+      <p>
+        Name: Dean Worrall
+        <br />
+        07777 777 777
+        <br />
+        Email: deanworrall@example.com
+      </p>
+      <p>
+        Elida Gibbs are always on the lookout for new players to join our squad.
+        Whether you're an experienced footballer or just looking to get back
+        into the game, we'd love to hear from you. We're a good bunch of lads
+        who enjoy competitive football, having a laugh, and supporting each
+        other both on and off the pitch. Come and be part of a welcoming and
+        passionate team!
+        <br />
+        <br />
+        The team holds weekly training sessions every Tuesday at Cockburn
+        School, Gipsy Lane, Beeston, Leeds, LS11 5TT, from 7:30 PM to 9:00 PM.
+        Matches kick off at 10:00 AM on Sundays, with our home games played at
+        Bedquilts Recreation Ground, Leeds, LS16 8BX.
+      </p>
+    </>
+  );
 }
 
 export default Contact;
