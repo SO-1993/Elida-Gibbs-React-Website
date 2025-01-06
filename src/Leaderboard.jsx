bbc41dcedb3dd4c9bd81ae1edf59d863281c13fc
Scott <scottobrien93@outlook.com>
2025-01-06 13:19:43 +0000

    functioning buttons for leaderboard that work on click

--- src/Leaderboard.jsx
+++ src/Leaderboard.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
 function Leaderboard() {
-  return <div>Board</div>;
+  const handleClick = (e) => {
+    console.log(e.target);
+  };
+  return (
+    <div className="board">
+      <h1 className="Leaderboard">Leaderboard</h1>
+      <div className="statistic">
+        <button onClick={handleClick} data-id="appearances">
+          Appearances
+        </button>
+        <button onClick={handleClick} data-id="goals">
+          Goals
+        </button>
+        <button onClick={handleClick} data-id="assists">
+          Assists
+        </button>
+        <button onClick={handleClick} data-id="clean-sheets">
+          Clean Sheets
+        </button>
+        <button onClick={handleClick} data-id="yellow-cards">
+          Yellow Cards
+        </button>
+        <button onClick={handleClick} data-id="red-cards">
+          Red Cards
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Leaderboard;
