599c59361f288dd18213ef1d9f146f00958e15d3
Scott <scottobrien93@outlook.com>
2025-01-06 13:50:25 +0000

    created players array to be used for leaderboard

---
+++ database.js
@@ -0,0 +1,165 @@
+const players = [
+  // Goalkeepers
+  {
+    name: "Adam Fenwick",
+    position: "Goalkeeper",
+    DOB: "N/A",
+    img: "utils/images/goalkeeper-placeholder.jpg",
+  },
+  {
+    name: "James Holding",
+    position: "Goalkeeper",
+    DOB: "N/A",
+    img: "utils/images/goalkeeper-placeholder.jpg",
+  },
+  {
+    name: "Tommy Ford",
+    position: "Goalkeeper",
+    DOB: "N/A",
+    img: "utils/images/goalkeeper-placeholder.jpg",
+  },
+
+  // Defenders
+  {
+    name: "Tom Hammond",
+    position: "Defender",
+    DOB: "N/A",
+    img: "utils/images/defender-placeholder.jpg",
+  },
+  {
+    name: "Joe Butler",
+    position: "Defender",
+    DOB: "N/A",
+    img: "utils/images/defender-placeholder.jpg",
+  },
+  {
+    name: "Scott O'Brien",
+    position: "Defender",
+    DOB: "N/A",
+    img: "utils/images/defender-placeholder.jpg",
+  },
+  {
+    name: "Dan Schackleton",
+    position: "Defender",
+    DOB: "N/A",
+    img: "utils/images/defender-placeholder.jpg",
+  },
+  {
+    name: "Joshua Barnett",
+    position: "Defender",
+    DOB: "N/A",
+    img: "utils/images/defender-placeholder.jpg",
+  },
+  {
+    name: "Cameron Robinson",
+    position: "Defender",
+    DOB: "N/A",
+    img: "utils/images/defender-placeholder.jpg",
+  },
+  {
+    name: "Jamie Fozard",
+    position: "Defender",
+    DOB: "N/A",
+    img: "utils/images/defender-placeholder.jpg",
+  },
+  {
+    name: "Tim Malkin",
+    position: "Defender",
+    DOB: "N/A",
+    img: "utils/images/defender-placeholder.jpg",
+  },
+
+  // Midfielders
+  {
+    name: "Danny Kreft",
+    position: "Midfielder",
+    DOB: "N/A",
+    img: "utils/images/midfielder-placeholder.jpg",
+  },
+  {
+    name: "Dan Edmonds",
+    position: "Midfielder",
+    DOB: "N/A",
+    img: "utils/images/midfielder-placeholder.jpg",
+  },
+  {
+    name: "Dean Worrall (C)",
+    position: "Midfielder",
+    DOB: "N/A",
+    img: "utils/images/midfielder-placeholder.jpg",
+  },
+  {
+    name: "Adam Franklin",
+    position: "Midfielder",
+    DOB: "N/A",
+    img: "utils/images/midfielder-placeholder.jpg",
+  },
+  {
+    name: "Jacob Darkes",
+    position: "Midfielder",
+    DOB: "N/A",
+    img: "utils/images/midfielder-placeholder.jpg",
+  },
+  {
+    name: "Arjun Varma",
+    position: "Midfielder",
+    DOB: "N/A",
+    img: "utils/images/midfielder-placeholder.jpg",
+  },
+  {
+    name: "Aran Harris",
+    position: "Midfielder",
+    DOB: "N/A",
+    img: "utils/images/midfielder-placeholder.jpg",
+  },
+  {
+    name: "Jai Narayan",
+    position: "Midfielder",
+    DOB: "N/A",
+    img: "utils/images/midfielder-placeholder.jpg",
+  },
+
+  // Attackers
+  {
+    name: "Oli Baron",
+    position: "Attacker",
+    DOB: "N/A",
+    img: "utils/images/attacker-placeholder.jpg",
+  },
+  {
+    name: "Jordan Moore",
+    position: "Attacker",
+    DOB: "N/A",
+    img: "utils/images/attacker-placeholder.jpg",
+  },
+  {
+    name: "Jordan Mercer",
+    position: "Attacker",
+    DOB: "N/A",
+    img: "utils/images/attacker-placeholder.jpg",
+  },
+  {
+    name: "Andy Pye",
+    position: "Attacker",
+    DOB: "N/A",
+    img: "utils/images/attacker-placeholder.jpg",
+  },
+  {
+    name: "Josh Robinson",
+    position: "Attacker",
+    DOB: "N/A",
+    img: "utils/images/attacker-placeholder.jpg",
+  },
+  {
+    name: "Cameron Liddell",
+    position: "Attacker",
+    DOB: "N/A",
+    img: "utils/images/midfielder-placeholder.jpg",
+  },
+  {
+    name: "Harry Whittaker",
+    position: "Attacker",
+    DOB: "N/A",
+    img: "utils/images/midfielder-placeholder.jpg",
+  },
+];
