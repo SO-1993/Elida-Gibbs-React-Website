58c17a054703d16ce7f347f922053196c6ae915e
Scott <scottobrien93@outlook.com>
2025-01-10 09:21:58 +0000

    styled Contact container / section so it reseembles Sponsors equivalent

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -5,30 +5,49 @@ function Contact() {
   return (
     <>
       <div>{<ImageCarousel />}</div>
-      <h1>Contact Elida Gibbs AFC</h1>
-      <p>
-        Name: Dean Worrall
-        <br />
-        07777 777 777
-        <br />
-        Email: deanworrall@example.com
-      </p>
-      <p>
-        Elida Gibbs are always on the lookout for new players to join our squad.
-        Following promotion last from Division 3 in the 2023/24 season, we are
-        ambitious to continue our jouney up the divisions. Whether you're an
-        experienced footballer, fresh from underage football, or looking to get
-        back into the game after a period of time out, we'd love to hear from
-        you. We're a good bunch of lads who enjoy competitive football, having a
-        laugh, and supporting each other both on and off the pitch. Come and be
-        part of a welcoming and passionate team!
-        <br />
-        <br />
-        The team holds weekly training sessions every Tuesday at Cockburn
-        School, Gipsy Lane, Beeston, Leeds, LS11 5TT, from 7:30 PM to 9:00 PM.
-        Matches kick off at 10:00 AM on Sundays, with our home games played at
-        Bedquilts Recreation Ground, Leeds, LS16 8BX.
-      </p>
+      <div className="contact-container">
+        <div className="contact-section">
+          <h2>Contact Elida Gibbs AFC</h2>
+          <p>
+            <strong>Name:</strong> Dean Worrall
+            <br />
+            <strong>Phone:</strong> 07777 777 777
+            <br />
+            <strong>Email:</strong> deanworrall@example.com
+          </p>
+          <h4>Key Information</h4>
+          <strong>Kick Off:</strong> Sunday (10 AM)
+          <br />
+          <strong>Home Pitch:</strong> Bedquilts Recreation Ground, Leeds, LS16
+          8BX
+          <br />
+          <strong>Manager:</strong> Adam Fenwick
+          <br />
+          <strong>Captain:</strong> Dean Worrall
+          <p>
+            Elida Gibbs are always on the lookout for new players to join our
+            squad. Following promotion from Division 3 in the 2023/24 season, we
+            are ambitious to continue our journey up the divisions.
+            <br />
+          </p>
+          <p>
+            Whether you're an experienced footballer, fresh from underage
+            football, or looking to get back into the game after a period of
+            time out, we'd love to hear from you. Our team is made up of all
+            ages so don't be shy! We're a good bunch of lads who enjoy
+            competitive football, having a laugh, and supporting each other both
+            on and off the pitch.
+            <br />
+          </p>
+          <p>
+            The team holds weekly training sessions every Tuesday at Cockburn
+            School, Gipsy Lane, Beeston, Leeds, LS11 5TT, from 7:30 PM to 9:00
+            PM. Matches kick off at 10:00 AM on Sundays, with our home games
+            played at Bedquilts Recreation Ground, Leeds, LS16 8BX.
+          </p>
+          <br />
+        </div>
+      </div>
     </>
   );
 }
